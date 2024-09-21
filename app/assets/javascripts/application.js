c31fd80ba0f9a8ff619d28925c29821b3abd4f1a
小西達也 <konishitatsuya@konishitatsuyanoMacBook-Air.local>
2024-09-21 11:28:18 +0900

    ミス

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,6 @@
-import "@hotwired/turbo-rails";
-import * as bootstrap from "bootstrap";
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import * as bootstrap from "bootstrap/dist/js/bootstrap"
+document.addEventListener("turbo:load", () => {
+    console.log("Turbo is working!");
+});
---
+++ public/assets/application-f5888601f44f527290c741b4abb92970d5d82bc215ce1adaabf4993586e3da8e.js
@@ -0,0 +1,4 @@
+import "@hotwired/turbo-rails";
+import * as bootstrap from "bootstrap/dist/js/bootstrap"
+import Rails from "@rails/ujs";
+Rails.start();
