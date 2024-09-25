2910ec983eb1ce93a31ec2db031b35b34724dc18
小西達也 <konishitatsuya@konishitatsuyanoMacBook-Air.local>
2024-09-25 17:26:53 +0900

    練習記録詳細

--- app/controllers/practice_records_controller.rb
+++ app/controllers/practice_records_controller.rb
@@ -1,6 +1,17 @@
 class PracticeRecordsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_practice_record, only: [:show, :edit, :update, :destroy]
   def index
     @practice_records = current_user.practice_records
+  end
+
+  def show
+    # @practice_recordはset_practice_recordメソッドでセットされる
+  end
+
+  private
+
+  def set_practice_record
+    @practice_record = current_user.practice_records.find(params[:id])
   end
 end
