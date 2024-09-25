658d680e1dc1aeeefe1273d6137746f442dd1232
小西達也 <konishitatsuya@konishitatsuyanoMacBook-Air.local>
2024-09-25 18:30:04 +0900

    練習記録作成

--- app/models/practice_record.rb
+++ app/models/practice_record.rb
@@ -2,5 +2,9 @@ class PracticeRecord < ApplicationRecord
   belongs_to :user
   # 今後practice_menuテーブルができたらリレーションを追加
    # belongs_to :practice_menu
+
+   validates :goal, presence: true
+   validates :date, presence: true
+   validates :comment, length: { maximum: 500 }
 end
   
